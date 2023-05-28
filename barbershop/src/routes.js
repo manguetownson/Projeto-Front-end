a622e16b6b03cc2fdcf6d3f249c347e654768dcb
MarcusLeão <marcuscarneironunes@gmail.com>
2023-05-28 15:48:33 -0300

    Vite React Project with Yarn, Redux and Redux-ToolKit

--- barbershop/src/routes.js
+++
@@ -1,5 +0,0 @@
-const Routes = () => {
-    return <h1>teste</h1>;
-};
-
-export default Routes;
--- barbershop/src/store/index.js
+++ barbershop/src/store/index.js
@@ -1,18 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
+import{configureStore} from '@reduxjs/toolkit'
 
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from "redux-devtools-extension";
+export default configureStore({
+    reducer:{}
 
-import rootReducer from "./rootReducer";
-import rootSaga from './rootSaga';
-
-const SagaMiddleware = createSagaMiddleware();
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTRENSION__ && window.__REDUX_DEVTOOLS_EXTRENSION__()
-? composeWithDevTools(applyMiddleware(SagaMiddleware))
-: applyMiddleware(SagaMiddleware)
-);
-
-SagaMiddleware.run(rootSaga);
-
-export default store;
+})
