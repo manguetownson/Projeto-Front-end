cee0d5a2a31441a2af2ccd31642386019576cff5
manguetownson <luna.artur1999@gmail.com>
2023-05-30 06:45:11 -0300

    inicio react-router

---
+++ barbershop/src/routes.jsx
@@ -0,0 +1,31 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import './styles.css';
+
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+import Agendamentos from './pages/Agendamentos';
+import Clientes from './pages/Clientes';
+
+
+const Routes1 = () => {
+    return (
+        <>
+        <Header />
+        <div className='container-fluid h-100'>
+            <div className='row h-100'>
+            <Router> 
+            <Sidebar />   
+             <Routes>
+                <Route path="/" element={Agendamentos}  />
+                <Route path="/clientes" element={Clientes} />
+             </Routes> 
+            </Router>       
+            </div>
+        </div>    
+        </>
+    );
+};
+
+export default Routes1;
