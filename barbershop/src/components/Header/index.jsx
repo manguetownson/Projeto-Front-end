f18bea8b09c1a859afd9a6c443f4b92a3c61b26d
manguetownson <luna.artur1999@gmail.com>
2023-05-30 06:44:35 -0300

    inicio do header

---
+++ barbershop/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return(
+        <header className="container-fluid d-flex justify-content-end">
+            <div className="d-flex align-items-center">
+                <div>
+                    <span>Barbearia EstiloMaloka</span>
+                    <small>Plano Anual</small>
+                </div>
+                <span className="mdi mdi-chevron-down"></span>
+            </div>    
+        </header>
+    );
+};
+
+export default Header;
