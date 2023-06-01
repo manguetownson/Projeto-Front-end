4864891b65326e7c7a24308f70e0b3bb2df2dd3d
MarcusLeão <marcuscarneironunes@gmail.com>
2023-06-01 18:08:20 -0300

    Side bar config

--- barbershop/src/components/Sidebar/index.jsx
+++ barbershop/src/components/Sidebar/index.jsx
@@ -1,18 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import logo from '../../assets/BarbeariaEstiloMaloka.png'
-const Sidebar = () => {
+const Sidebar = ({location}) => {
     return(
         <sidebar className="col-2 h-100">
             <img src={logo} className='img-fluid px-3 py-4'/>
-            <ul>
+            <ul className='p-0 m-0'>
                 <li>
-                    <Link to="/">
+                    <Link to="/" className={location.pathname === '/' ? 'active':''}>
                     <span className='mdi mdi-calendar-check'></span>
                     <text>Agendamentos</text>
                     </Link>
                 </li>
                 <li>
-                <Link to="/clientes">
+                <Link to="/clientes" className={location.pathname === '/clientes' ? 'active':''}>
                 <span className='mdi mdi-account-multiple'></span>
                     <text>Clientes</text>
                 </Link>
@@ -22,4 +22,4 @@ const Sidebar = () => {
     );
 };
 
-export default Sidebar;
+export default withRouter(Sidebar);
